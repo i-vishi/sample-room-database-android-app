e463246d255e5c7158001bc17460d7d694734c81
i-vishi <rr.vishalgaur@gmail.com>
2021-04-08 10:43:44 +0530

    modified Nav Graph

--- app/src/main/java/com/vishalgaur/sampleroomdatabaseapp/EditFragment.kt
+++ app/src/main/java/com/vishalgaur/sampleroomdatabaseapp/EditFragment.kt
@@ -11,21 +11,21 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class InfoFragment : Fragment() {
+class EditFragment : Fragment() {
 
 	override fun onCreateView(
 			inflater: LayoutInflater, container: ViewGroup?,
 			savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_info, container, false)
+		return inflater.inflate(R.layout.fragment_edit, container, false)
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
 		view.findViewById<Button>(R.id.button_second).setOnClickListener {
-			findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+			findNavController().navigate(R.id.actionGoBackToHome)
 		}
 	}
 }
