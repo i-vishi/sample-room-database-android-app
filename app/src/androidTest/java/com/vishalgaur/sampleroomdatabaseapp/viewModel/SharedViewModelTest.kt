3256c1acced425d30d60f92990908ac8bfc55024
i-vishi <rr.vishalgaur@gmail.com>
2021-04-13 15:10:01 +0530

    tested shared view model

--- app/src/androidTest/java/com/vishalgaur/sampleroomdatabaseapp/viewModel/SharedViewModelTest.kt
+++ app/src/androidTest/java/com/vishalgaur/sampleroomdatabaseapp/viewModel/SharedViewModelTest.kt
@@ -3,6 +3,8 @@ package com.vishalgaur.sampleroomdatabaseapp.viewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.vishalgaur.sampleroomdatabaseapp.DataStatus
+import com.vishalgaur.sampleroomdatabaseapp.SearchErrors
 import com.vishalgaur.sampleroomdatabaseapp.ViewErrors
 import com.vishalgaur.sampleroomdatabaseapp.getOrAwaitValue
 import org.hamcrest.Matchers.*
@@ -14,68 +16,106 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SharedViewModelTest {
-    private lateinit var sharedViewModel: SharedViewModel
+	private lateinit var sharedViewModel: SharedViewModel
 
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+	@get:Rule
+	var instantExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    fun setUp() {
-        sharedViewModel = SharedViewModel(ApplicationProvider.getApplicationContext())
-    }
+	@Before
+	fun setUp() {
+		sharedViewModel = SharedViewModel(ApplicationProvider.getApplicationContext())
+	}
 
-//    @Test
-//    fun submitData_noData_returnsEmptyError() {
-//        val name = ""
-//        val email = ""
-//        val mob = ""
-//        val dob = ""
-//
-//        sharedViewModel.submitData(name, email, mob, dob)
-//        assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMPTY))
-//    }
-//
-//    @Test
-//    fun submitData_allValid_returnsNoError() {
-//        val name = "Vishal"
-//        val email = "   vishal@mail.com "
-//        val mob = "  7056897878"
-//        val dob = "01/01/1999"
-//
-//        sharedViewModel.submitData(name, email, mob, dob)
-//        assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.NONE))
-//    }
-//
-//    @Test
-//    fun submitData_invalidEmail_returnsEmailError() {
-//        val name = "Vishal"
-//        val email = "   vishal@mailom "
-//        val mob = "  7056897811"
-//        val dob = "01/01/1999"
-//
-//        sharedViewModel.submitData(name, email, mob, dob)
-//        assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMAIL))
-//    }
-//
-//    @Test
-//    fun submitData_invalidMobile_returnsMobileError() {
-//        val name = "Vishal"
-//        val email = "   vishal@mail.com "
-//        val mob = "  70568911 "
-//        val dob = "01/01/1999"
-//
-//        sharedViewModel.submitData(name, email, mob, dob)
-//        assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_MOBILE))
-//    }
-//
-//    @Test
-//    fun submitData_invalidEmailMobile_returnsEmailMobileError() {
-//        val name = "Vishal"
-//        val email = "   vishalailcom "
-//        val mob = "  70568911 "
-//        val dob = "01/01/1999"
-//
-//        sharedViewModel.submitData(name, email, mob, dob)
-//        assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMAIL_MOBILE))
-//    }
+	@Test
+	fun submitData_noData_returnsEmptyError() {
+		val id = ""
+		val name = ""
+		val email = ""
+		val mob = ""
+		val dob = ""
+
+		sharedViewModel.submitData(id, name, email, mob, dob)
+		assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMPTY))
+	}
+
+	@Test
+	fun submitData_allValid_returnsNoError() {
+		val id = "45"
+		val name = "Vishal"
+		val email = "   vishal@mail.com "
+		val mob = "  7056897878"
+		val dob = "01/01/1999"
+
+		sharedViewModel.submitData(id, name, email, mob, dob)
+		assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.NONE))
+	}
+
+	@Test
+	fun submitData_invalidEmail_returnsEmailError() {
+		val id = "26"
+		val name = "Vishal"
+		val email = "   vishal@mailom "
+		val mob = "  7056897811"
+		val dob = "01/01/1999"
+
+		sharedViewModel.submitData(id, name, email, mob, dob)
+		assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMAIL))
+	}
+
+	@Test
+	fun submitData_invalidMobile_returnsMobileError() {
+		val id = "78"
+		val name = "Vishal"
+		val email = "   vishal@mail.com "
+		val mob = "  70568911 "
+		val dob = "01/01/1999"
+
+		sharedViewModel.submitData(id, name, email, mob, dob)
+		assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_MOBILE))
+	}
+
+	@Test
+	fun submitData_invalidEmailMobile_returnsEmailMobileError() {
+		val id = "123"
+		val name = "Vishal"
+		val email = "   vishalailcom "
+		val mob = "  70568911 "
+		val dob = "01/01/1999"
+
+		sharedViewModel.submitData(id, name, email, mob, dob)
+		assertThat(sharedViewModel.errorStatus.getOrAwaitValue(), `is`(ViewErrors.ERR_EMAIL_MOBILE))
+	}
+
+	@Test
+	fun searchData_null_returnsError() {
+		val id = ""
+		sharedViewModel.searchData(id)
+		assertThat(sharedViewModel.searchErrStatus.getOrAwaitValue(), `is`(SearchErrors.ERR_EMPTY))
+	}
+
+	@Test
+	fun searchData_invalid_returnsError() {
+		val id = "00654jfv45"
+		sharedViewModel.searchData(id)
+		assertThat(sharedViewModel.searchErrStatus.getOrAwaitValue(), `is`(SearchErrors.ERR_INVALID))
+	}
+
+	@Test
+	fun searchData_valid_returnsNoError() {
+		val id = "123"
+		sharedViewModel.searchData(id)
+		assertThat(sharedViewModel.searchErrStatus.getOrAwaitValue(), `is`(isOneOf(SearchErrors.ERR_NOT_FOUND, SearchErrors.NONE)))
+	}
+
+	@Test
+	fun setStatus_true_returnsLoaded() {
+		sharedViewModel.setStatus(true)
+		assertThat(sharedViewModel.status.getOrAwaitValue(), `is`(DataStatus.LOADED))
+	}
+
+	@Test
+	fun setStatus_false_returnsEmpty() {
+		sharedViewModel.setStatus(false)
+		assertThat(sharedViewModel.status.getOrAwaitValue(), `is`(DataStatus.EMPTY))
+	}
 }
--- app/src/main/java/com/vishalgaur/sampleroomdatabaseapp/viewModel/SharedViewModel.kt
+++ app/src/main/java/com/vishalgaur/sampleroomdatabaseapp/viewModel/SharedViewModel.kt
@@ -15,129 +15,130 @@ import java.io.IOException
 private const val TAG = "SharedViewModel"
 
 class SharedViewModel(application: Application) :
-    AndroidViewModel(application) {
+		AndroidViewModel(application) {
 
-    private val usersRepository = UsersRepository(UserDatabase.getInstance(application))
+	private val usersRepository = UsersRepository(UserDatabase.getInstance(application))
 
-    var userData: LiveData<UserData?>
+	var userData: LiveData<UserData?>
 
-    private val _searchedData = MutableLiveData<UserData?>()
-    val searchedData: LiveData<UserData?> get() = _searchedData
+	private val _searchedData = MutableLiveData<UserData?>()
+	val searchedData: LiveData<UserData?> get() = _searchedData
 
-    private val _status = MutableLiveData<DataStatus>()
-    val status: LiveData<DataStatus> get() = _status
+	private val _status = MutableLiveData<DataStatus>()
+	val status: LiveData<DataStatus> get() = _status
 
-    private val _errorStatus = MutableLiveData<ViewErrors>()
-    val errorStatus: LiveData<ViewErrors> get() = _errorStatus
+	private val _errorStatus = MutableLiveData<ViewErrors>()
+	val errorStatus: LiveData<ViewErrors> get() = _errorStatus
 
-    private val _searchErrStatus = MutableLiveData<SearchErrors>()
-    val searchErrStatus: LiveData<SearchErrors> get() = _searchErrStatus
+	private val _searchErrStatus = MutableLiveData<SearchErrors>()
+	val searchErrStatus: LiveData<SearchErrors> get() = _searchErrStatus
 
-    init {
-        userData = MutableLiveData()
-        refreshDataFromRepository()
-    }
+	init {
+		userData = MutableLiveData()
+        _status.value = DataStatus.EMPTY
+        _errorStatus.value = ViewErrors.NONE
+        _searchErrStatus.value = SearchErrors.NONE
+		refreshDataFromRepository()
+	}
 
-    private fun refreshDataFromRepository() {
-        viewModelScope.launch {
-            _status.value = DataStatus.LOADING
-            _errorStatus.value = ViewErrors.NONE
-            _searchErrStatus.value = SearchErrors.NONE
+	private fun refreshDataFromRepository() {
+		viewModelScope.launch {
+			setData()
 
-            setData()
+			try {
+				_status.value = DataStatus.LOADING
+				Log.d(TAG, "Getting data from network...")
+				usersRepository.refreshData()
+			} catch (networkError: IOException) {
+				Log.d(TAG, "Not able to get data from network!")
+			}
 
-            try {
-                Log.d(TAG, "Getting data from network...")
-                usersRepository.refreshData()
-            } catch (networkError: IOException) {
-                Log.d(TAG, "Not able to get data from network!")
-            }
+			setStatus(userData.value != null)
+		}
+	}
 
-            setStatus(userData.value != null)
-        }
-    }
+	private fun setData() {
+		_status.value = DataStatus.LOADING
+		Log.d(TAG, "Getting data from Room...")
+		viewModelScope.launch {
+			userData = usersRepository.uData
+		}
+	}
 
-    private fun setData() {
-        Log.d(TAG, "Getting data from Room...")
-        viewModelScope.launch {
-            userData = usersRepository.uData
-        }
-    }
+	fun setStatus(dataFound: Boolean) {
+		if (dataFound) {
+			Log.d(TAG, "Data found: ${userData.value?.userName}")
+			_status.value = DataStatus.LOADED
+		} else {
+			Log.d(TAG, "No Data Found!")
+			_status.value = DataStatus.EMPTY
+		}
+	}
 
-    fun setStatus(dataFound: Boolean) {
-        if (dataFound) {
-            Log.d(TAG, "Data found: ${userData.value?.userName}")
-            _status.value = DataStatus.LOADED
-        } else {
-            Log.d(TAG, "No Data Found!")
-            _status.value = DataStatus.EMPTY
-        }
-    }
+	fun searchData(userId: String) {
+		Log.d(TAG, "QUERY: $userId")
+		when {
+			userId.isBlank() -> {
+				_searchErrStatus.value = SearchErrors.ERR_EMPTY
+			}
+			userId != userId.toLongOrNull().toString() -> {
+				_searchErrStatus.value = SearchErrors.ERR_INVALID
+			}
+			else -> {
+				getData(userId.toLong())
+			}
+		}
+	}
 
-    fun searchData(userId: String) {
-        Log.d(TAG, "QUERY: $userId")
-        when {
-            userId.isBlank() -> {
-                _searchErrStatus.value = SearchErrors.ERR_EMPTY
-            }
-            userId != userId.toLong().toString() -> {
-                _searchErrStatus.value = SearchErrors.ERR_INVALID
-            }
-            else -> {
-                getData(userId.toLong())
-            }
-        }
-    }
-
-    fun submitData(id: String, name: String, email: String, mobile: String, dob: String) {
-        if (id.isBlank() || name.isBlank() || email.isBlank() || mobile.isBlank() || dob.isBlank()) {
-            _status.value = DataStatus.EMPTY
-            _errorStatus.value = ViewErrors.ERR_EMPTY
-        } else {
-            var err = ERR_INIT
-            if (!isEmailValid(email)) {
-                err += ERR_EMAIL
-            }
-            if (!isPhoneValid(mobile)) {
-                err += ERR_MOBILE
-            }
-            when (err) {
+	fun submitData(id: String, name: String, email: String, mobile: String, dob: String) {
+		if (id.isBlank() || name.isBlank() || email.isBlank() || mobile.isBlank() || dob.isBlank()) {
+			_status.value = DataStatus.EMPTY
+			_errorStatus.value = ViewErrors.ERR_EMPTY
+		} else {
+			var err = ERR_INIT
+			if (!isEmailValid(email)) {
+				err += ERR_EMAIL
+			}
+			if (!isPhoneValid(mobile)) {
+				err += ERR_MOBILE
+			}
+			when (err) {
                 ERR_INIT -> {
                     _errorStatus.value = ViewErrors.NONE
                     _status.value = DataStatus.LOADED
                     val newData =
-                        UserData(id.toLong(), name.trim(), email.trim(), mobile.trim(), dob)
+                            UserData(id.toLong(), name.trim(), email.trim(), mobile.trim(), dob)
                     insertData(newData)
                 }
                 (ERR_INIT + ERR_EMAIL) -> _errorStatus.value = ViewErrors.ERR_EMAIL
                 (ERR_INIT + ERR_MOBILE) -> _errorStatus.value = ViewErrors.ERR_MOBILE
                 (ERR_INIT + ERR_EMAIL + ERR_MOBILE) -> _errorStatus.value =
-                    ViewErrors.ERR_EMAIL_MOBILE
-            }
-        }
-    }
+                        ViewErrors.ERR_EMAIL_MOBILE
+			}
+		}
+	}
 
-    fun clearData() {
-        viewModelScope.launch {
-            _status.value = DataStatus.EMPTY
-            usersRepository.clearData()
-        }
-    }
+	fun clearData() {
+		viewModelScope.launch {
+			_status.value = DataStatus.EMPTY
+			usersRepository.clearData()
+		}
+	}
 
-    private fun insertData(newData: UserData) {
-        viewModelScope.launch {
-            _status.value = DataStatus.EMPTY
-            usersRepository.insertData(newData)
-        }
-    }
+	private fun insertData(newData: UserData) {
+		viewModelScope.launch {
+			_status.value = DataStatus.EMPTY
+			usersRepository.insertData(newData)
+		}
+	}
 
-    private fun getData(uId: Long) {
-        viewModelScope.launch {
-            _searchErrStatus.value = SearchErrors.NONE
-            _searchedData.value = usersRepository.uDataById(uId)
-            if (_searchedData.value == null) {
-                _searchErrStatus.value = SearchErrors.ERR_NOT_FOUND
-            }
-        }
-    }
+	private fun getData(uId: Long) {
+		_searchErrStatus.value = SearchErrors.NONE
+		viewModelScope.launch {
+			_searchedData.value = usersRepository.uDataById(uId)
+			if (_searchedData.value == null) {
+				_searchErrStatus.value = SearchErrors.ERR_NOT_FOUND
+			}
+		}
+	}
 }
